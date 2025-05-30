1d600ecdcd9ef87ea41475831a1209ba6cb1a879
436lcy <bryan@agencyone.us>
2025-05-30 17:28:32 +0800

    Initial Commit

---
+++ MyApp/MyAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyAppApp.swift
+//  MyApp
+//
+//  Created by 李晨阳 on 2025/5/30.
+//
+
+import SwiftUI
+
+@main
+struct MyAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MyAppTests/MyAppTests.swift
@@ -0,0 +1,16 @@
+//
+//  MyAppTests.swift
+//  MyAppTests
+//
+//  Created by 李晨阳 on 2025/5/30.
+//
+
+import Testing
+
+struct MyAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
