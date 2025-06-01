e30c89dccd026ea52f9e946bd1b357eb62378e0f
436lcy <bryan@agencyone.us>
2025-06-01 15:38:43 +0800

    Add duplicate greeting message in Chinese to ContentView

--- MyApp/ContentView.swift
+++ MyApp/ContentView.swift
@@ -15,6 +15,8 @@ struct ContentView: View {
                 .foregroundStyle(.tint)
             Text("Hello, world!")
             Text("我的第一次自动化测试pull request")
+            Text("我的第一次自动化测试pull request")
+            Text("我的第一次自动化测试pull request")
         }
         .padding()
     }
